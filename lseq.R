f9bd170a0df3197490fd960f044647f375e87ac3
bblonder <bblonder@gmail.com>
2021-12-26 15:05:02 -0800

    removing old files

--- lseq.R
+++
@@ -1,5 +0,0 @@
-lseq <- function(from, to, length.out) {
-  # logarithmic spaced sequence
-  # blatantly stolen from library("emdbook"), because need only this
-  exp(seq(log(from), log(to), length.out = length.out))
-}
