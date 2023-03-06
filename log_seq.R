d907d05f6253bd5eb618246bfe068a4ed7fe1baa
michaelhlim <5422392+michaelhlim@users.noreply.github.com>
2023-03-06 13:21:05 -0800

    Restructuring part 2

--- log_seq.R
+++
@@ -1,5 +0,0 @@
-log_seq <- function(from, to, length.out) {
-  # logarithmic spaced sequence
-  # blatantly stolen from library("emdbook"), because need only this
-  exp(seq(log(from), log(to), length.out = length.out))
-}
